6c0d9d84c1ee300367d7f8b753f2adf81b165479
Tupalski Ilya <itupalski@gmail.com>
2023-06-06 16:06:17 +0400

    feat: add type object and array (Tue Jun  6 16:06:17 +04 2023)

--- ts-files/type.ts
+++ ts-files/type.ts
@@ -1,41 +1,66 @@
-// String
-let userName: string = 'Ilya';
-
-// Number
-let userAge: number = 19;
-
-// Boolean
-let isPeople: boolean = true;
-
-// Function
-function logMsg (userName: string, age: number, isPeople: boolean): string {
-  if (isPeople) {
-    return `${userName} ${age} years old`;
-  } else {
-    return 'Error';
-  }
-}
-logMsg(userName, userAge, isPeople);
-
-// Function arrow
-const logMsgArrow = (userName: string, age: number, isPeople: boolean): string => {
-  if (isPeople) {
-    return `${userName} ${age} years old`;
-  } else {
-    return 'Error';
-  }
-}
+// // String
+// let userName: string = 'Ilya';
+//
+// // Number
+// let userAge: number = 19;
+//
+// // Boolean
+// let isPeople: boolean = true;
+//
+// // Function
+// function logMsg (userName: string, age: number, isPeople: boolean): string {
+//   if (isPeople) {
+//     return `${userName} ${age} years old`;
+//   } else {
+//     return 'Error';
+//   }
+// }
+// logMsg(userName, userAge, isPeople);
+//
+// // Function arrow
+// const logMsgArrow = (userName: string, age: number, isPeople: boolean): string => {
+//   if (isPeople) {
+//     return `${userName} ${age} years old`;
+//   } else {
+//     return 'Error';
+//   }
+// }
 // console.log(logMsgArrow(userName, userAge, isPeople));
+//
+// // Type Any (JSON)
+// let salary;
+// salary = 5000;
+//
+// const userData = '{"bol": true, "num": 40}'
+// const userObjJ: {
+//   bol: true,
+//   num: 40
+// } = JSON.parse(userData);
+// // console.log(userObjJ);
+//
+// // Type never
+// const createError = (msg: string): never => {
+//   throw new Error(msg);
+// };
+// createError('Error');
 
-// Type Any (JSON)
-let salary;
-salary = 5000;
+// Object destructuring
+const userObj = {
+  userName: 'Ilya',
+  userAge: 19,
+  isPeople: true
+}
+const logMsgObj = ({userName, userAge, isPeople}: {userName: string, userAge: number, isPeople: boolean}): string => {
+  if (isPeople) {
+    return `${userName} ${userAge} years old`;
+  } else {
+    return 'Error';
+  }
+}
+console.log(logMsgObj(userObj));
 
-const userData = '{"sdfsd": true, "dfgfd": 40}'
-
-const userObj: {
-  sdfsd: true,
-  dfgfd: 40
-} = JSON.parse(userData);
-
-// console.log(userObj);
+// Array
+const departments: string[] = ['dev', 'design', 'figma'];
+const nums: number[] = [1, 4, 4];
+const Arr: number[][] = [[1,4], [2, 5]];
+const [first] = nums;
