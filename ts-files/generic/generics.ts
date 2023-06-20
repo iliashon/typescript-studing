e43357e6294a462efe0a0e86ffc91cb39dfff7d6
Tupalski Ilya <itupalski@gmail.com>
2023-06-20 12:44:09 +0400

    feat: add task generic (Tue Jun 20 12:44:09 +04 2023)

--- ts-files/generic/generics.ts
+++ ts-files/generic/generics.ts
@@ -23,42 +23,89 @@
 //   desing: 'ten'
 // }
 
-function processing<T, S>(data: T[], options: S): string {
-  data.length;
-  switch (typeof data) {
-    case "string":
-      return `${data}, speed: ${options}`;
-      break;
-    case "number":
-      return `${data}, speed: ${options}`;
-      break;
-    default:
-      return "Not valid";
-  }
+// function processing<T, S>(data: T[], options: S): string {
+//   data.length;
+//   switch (typeof data) {
+//     case "string":
+//       return `${data}, speed: ${options}`;
+//       break;
+//     case "number":
+//       return `${data}, speed: ${options}`;
+//       break;
+//     default:
+//       return "Not valid";
+//   }
+// }
+//
+// const res1 = processing([1], 'slo');
+// const res2 = processing(['1'], 'slo');
+//
+// const res3 = processing<number, string>([10], 'slo');
+//
+// function processings<T>(data: T): T {
+//   return data;
+// }
+//
+// let newFunc: <T>(data: T) => T = processings;
+//
+// interface  DataServer {
+//   process: <T>(data: T) => T;
+// }
+//
+// const server: DataServer = {
+//   // process(data) {
+//   //   console.log(data);
+//   //   return data;
+//   // }
+//   // process: <T>(data: T) => {
+//   //   return data;
+//   // }
+//   process: processings
+// }
+
+import * as stream from "stream";
+
+interface ProcessingFn {
+  <T>(data: T): T;
 }
 
-const res1 = processing([1], 'slo');
-const res2 = processing(['1'], 'slo');
-
-const res3 = processing<number, string>([10], 'slo');
-
-function processings<T>(data: T): T {
+function processing<T>(data: T): T {
   return data;
 }
 
-let newFunc: <T>(data: T) => T = processings;
+let newFunc: ProcessingFn = processing;
 
-interface  DataServer {
-  process: <T>(data: T) => T;
+type Smth<T> = T;
+
+const num: Smth<number> = 5;
+
+interface ParentsOfUser {
+  mother: string, father: string,
 }
 
-const server: DataServer = {
-  // process(data) {
-  //   console.log(data);
-  //   return data;
-  // }
-  // process: <T>(data: T) => {
-  //   return data;
-  // }
-  process: processings
+interface User<ParentsData extends ParentsOfUser> {
+  login: string;
+  age: number;
+  parents: ParentsData;
 }
+
+const user: User<{mother: string, father: string, married: string}> = {
+  login: 'str',
+  age: 54,
+  parents: {
+    mother: 'anna',
+    father: 'alex',
+    married: 'no',
+  }
+}
+
+type OrNull<Type> = Type | null;
+type OneOrMany<Type> = Type | Type[];
+
+const data: OneOrMany<number[]> = [5];
+
+const deposit = <T extends number | string>(amount: T): T => {
+  console.log(`reg ${amount}`);
+  return amount;
+}
+deposit(500);
